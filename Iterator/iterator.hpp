29b375b988a22ad53f6a5e7040234204ef15587a
Mohamed Bifenzi <mbifenzi@e1r3p4.1337.ma>
2022-06-20 05:47:36 +0100

    errors handled

--- Iterator/iterator.hpp
+++ Iterator/iterator.hpp
@@ -27,10 +27,35 @@ namespace ft
 			iterator_op &operator-=(difference_type n) { _ptr -= n; return *this; };
 			iterator_op operator++(int) { iterator_op tmp(*this); _ptr++; return tmp; };
 			iterator_op operator--(int) { iterator_op tmp(*this); _ptr--; return tmp; };
-			iterator_op operator+(difference_type n) { iterator_op tmp(*this); tmp._ptr += n; return tmp; };
-			iterator_op operator-(difference_type n) { iterator_op tmp(*this); tmp._ptr -= n; return tmp; };
-			difference_type operator-(iterator_op const &it) { return _ptr - it._ptr; };
-			difference_type operator+(iterator_op const &it) { return _ptr + it._ptr; };
+ 			difference_type operator-(iterator_op it) const
+            {
+                return (this->_ptr - it._ptr);
+            }
+            friend iterator_op operator+(int nb, const iterator_op& it)
+            {
+                iterator_op newIt(it);
+                return (newIt += nb);
+            }
+
+            friend iterator_op operator-(int nb, const iterator_op& it)
+            {
+                iterator_op newIt(it);
+                return (newIt -= nb);
+            }
+            iterator_op operator+(int nb) const
+            {
+                iterator_op it(*this);
+                
+                add(it._ptr, nb);
+                return (it);
+            }
+              iterator_op operator-(int nb) const
+            {
+                iterator_op it(*this);
+                
+                sub(it._ptr, nb);
+                return (it);
+            }
 
 			pointer operator->() { return _ptr; };
 			bool operator==(iterator_op const &it) { return _ptr == it._ptr; };
@@ -42,9 +67,42 @@ namespace ft
 			reference operator[](difference_type n) { return _ptr[n]; };
 			reference operator*() const { return *_ptr; };
 			pointer operator&() const { return _ptr; };
+			 void add(T*& val, long nb) const
+            {
+                int mov;
+
+                
+                if (nb > 0)
+                    mov = 1;
+                else
+                    mov = -1;
+                if (nb < 0)
+                nb *= -1;
+                for (; nb > 0; --nb)
+                val += mov;
+            }
+            void sub(T*& val, long nb) const
+            {
+                int mov;
+
+                
+                if (nb > 0)
+                    mov = -1;
+                else
+                    mov = 1;
+                if (nb < 0)
+                nb *= -1;
+                for (; nb > 0; --nb)
+                val  += mov;
+            }
 		// operator iterator<const value_type>()
         // {
         //     return iterator<const value_type>(_ptr);
         // }
     };
+	template <typename U>
+    iterator_op<U> operator+(std::ptrdiff_t n, ft::iterator_op<U> it)
+    {
+        return it + n;
+    }
 };
--- Iterator/reverse_iterator.hpp
+++ Iterator/reverse_iterator.hpp
@@ -28,9 +28,18 @@ namespace ft
             reverse_iterator &operator-=(difference_type n) { _it += n; return *this; }
             reverse_iterator operator++(int) { reverse_iterator tmp(*this); _it--; return tmp; }
             reverse_iterator operator--(int) { reverse_iterator tmp(*this); _it++; return tmp; }
-            reverse_iterator operator+(difference_type n) { reverse_iterator tmp(*this); tmp._it -= n; return tmp; }
-            reverse_iterator operator-(difference_type n) { reverse_iterator tmp(*this); tmp._it += n; return tmp; }
-            difference_type operator-(reverse_iterator const &it) { return it._it - _it; }
+
+
+			reverse_iterator operator+ (difference_type n) const
+			{
+				reverse_iterator tmp(_it - n);
+				return (tmp);
+			}
+            			reverse_iterator operator- (difference_type n) const
+			{
+				reverse_iterator tmp(_it + n);
+				return (tmp);
+			}
             pointer operator->() { return &*_it; }
             reference operator*() { return *(_it - 1); }
             reference operator[](difference_type n) { return _it[-n]; }
@@ -80,7 +89,17 @@ namespace ft
     }
 
 
-
+    template <class Iterator>
+	reverse_iterator<Iterator> operator+ (typename reverse_iterator<Iterator>::difference_type n, const reverse_iterator<Iterator>& rev_it)
+	{
+		return (rev_it + n);
+	}
+	//operator -
+	template <class Iterator>
+	typename reverse_iterator<Iterator>::difference_type operator- (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs)
+	{
+		return (rhs.base() - lhs.base());
+	}
     
 
 }
